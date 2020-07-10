0d32f042f6f3857e61b7f9022ae8b129a3ddf24d
Forkbabu <sayantandas30011998@gmail.com>
2020-07-10 10:17:16 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ import setuptools
 with open('README.md', 'r') as f:
     long_description = f.read()
 
-setuptools.setup(name='tf_notification_callback',
-      description='Receive notifications about your model training anywhere you want!',
+setuptools.setup(name='keras-notify',
+      description='Keras Model notifier on Telegram and Slack',
       long_description=long_description,
       long_description_content_type="text/markdown",
-      version='0.2',
-      url='https://github.com/techytushar/tf_notification_callback',
-      author='Tushar Mittal',
-      author_email='chiragmittal.mittal@gmail.com',
+      version='0.0.1',
+      url='https://github.com/forkbabu/keras-notify',
+      author='Sayantan Das',
+      author_email='sayantandas30011998@gmail.com',
       license='GNU General Public License v3 (GPLv3)',
       classifiers=[
           'Development Status :: Beta',
@@ -18,11 +18,10 @@ setuptools.setup(name='tf_notification_callback',
           'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
           'Programming Language :: Python 3'
       ],
-      keywords='api python keras training callback tensorflow machine learning deep learning nlp',
+      keywords='api python keras training callback tensorflow notifications deep learning nlp',
       install_requires=[
         'python-telegram-bot>=12',
         'matplotlib>=3',
-        'keras>=2.3',
         'tensorflow>=2'
         'requests>=2'
       ],
