bae0872f7827111c58f34e8e9ef2a454fa595c85
Forkbabu <sayantandas30011998@gmail.com>
2020-07-10 10:30:20 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,30 +1,29 @@
 import setuptools
 
-with open('README.md', 'r') as f:
-    long_description = f.read()
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
-setuptools.setup(name='keras-notify',
-      description='Keras Model notifier on Telegram and Slack',
-      long_description=long_description,
-      long_description_content_type="text/markdown",
-      version='0.0.1',
-      url='https://github.com/forkbabu/keras-notify',
-      author='Sayantan Das',
-      author_email='sayantandas30011998@gmail.com',
-      license='GNU General Public License v3 (GPLv3)',
-      classifiers=[
-          'Development Status :: Beta',
-          'Intended Audience :: Developers',
-          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-          'Programming Language :: Python 3'
-      ],
-      keywords='api python keras training callback tensorflow notifications deep learning nlp',
-      install_requires=[
-        'python-telegram-bot>=12',
-        'matplotlib>=3',
-        'tensorflow>=2'
-        'requests>=2'
-      ],
-      packages=setuptools.find_packages(),
-      python_requires='>=3.6'
+setuptools.setup(
+    name="keras-notify-ucalyptus", # Replace with your own username
+    version="0.0.1",
+    author="Sayantan Das",
+    author_email="sayantandas30011998@gmail.com",
+    description="A small notification package providing keras events on Telegram or Slack",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/forkbabu/keras-notify",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    keywords='api python keras training callback tensorflow notifications deep learning nlp',
+    install_requires=[
+      'python-telegram-bot>=12',
+      'matplotlib>=3',
+      'tensorflow>=2'
+      'requests>=2'
+    ],    
 )
